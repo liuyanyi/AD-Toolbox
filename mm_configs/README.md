394f89510393edf20c34d388be56593852b3cfeb
liuyanyi <wolfsonliu@163.com>
2022-08-24 14:50:33 +0800

    add mm configs

---
+++ mm_configs/README.md
@@ -0,0 +1,10 @@
+This directory contains the config files in MMDet and MMRotate for quick reference.
+
+此目录下包含MMDet和MMRotate中的配置文件，便于快速引用。
+
+### Version
+
+| Project  |                           version                            |
+|:--------:|:------------------------------------------------------------:|
+|  MMDet   |                            2.25.1                            |
+| MMRotate | [fork ver](https://github.com/liuyanyi/mmrotate/tree/ryolox) |
---
+++ mm_configs/mmrotate/rotated_retinanet/rotated_retinanet_obb_r50_fpn_3x_hrscdota_le90.py
@@ -0,0 +1,126 @@
+_base_ = [
+    '../_base_/datasets/dotav1.py', '../_base_/schedules/schedule_6x.py',
+    '../_base_/default_runtime.py'
+]
+
+angle_version = 'le90'
+model = dict(
+    type='RotatedRetinaNet',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        zero_init_residual=False,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch',
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        start_level=1,
+        add_extra_convs='on_input',
+        num_outs=5),
+    bbox_head=dict(
+        type='RotatedRetinaHead',
+        num_classes=1,
+        in_channels=256,
+        stacked_convs=4,
+        feat_channels=256,
+        assign_by_circumhbbox=None,
+        anchor_generator=dict(
+            type='RotatedAnchorGenerator',
+            octave_base_scale=4,
+            scales_per_octave=3,
+            ratios=[1.0, 0.5, 2.0],
+            strides=[8, 16, 32, 64, 128]),
+        bbox_coder=dict(
+            type='DeltaXYWHAOBBoxCoder',
+            angle_range=angle_version,
+            norm_factor=None,
+            edge_swap=True,
+            proj_xy=True,
+            target_means=(.0, .0, .0, .0, .0),
+            target_stds=(1.0, 1.0, 1.0, 1.0, 1.0)),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+    train_cfg=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.4,
+            min_pos_iou=0,
+            ignore_iof_thr=-1,
+            iou_calculator=dict(type='RBboxOverlaps2D')),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        nms_pre=2000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(iou_thr=0.1),
+        max_per_img=2000))
+
+dataset_type = 'DOTADataset'
+data_root = '/datasets/hrsc/FullDataSet/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RResize', img_scale=(800, 800)),
+    dict(type='RRandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(800, 800),
+        flip=False,
+        transforms=[
+            dict(type='RResize'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=8,
+    train=dict(
+        type=dataset_type,
+        classes=('ship', ),
+        ann_file=data_root + 'test/annfiles/',
+        img_prefix=data_root + 'test/images/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        classes=('ship', ),
+        ann_file=data_root + 'test/annfiles/',
+        img_prefix=data_root + 'test/images/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        classes=('ship', ),
+        ann_file=data_root + 'test/annfiles/',
+        img_prefix=data_root + 'test/images/',
+        pipeline=test_pipeline))
+
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+
+evaluation = dict(interval=6, metric='mAP')
+checkpoint_config = dict(interval=6)
